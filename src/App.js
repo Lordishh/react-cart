325ff3b40216c12185de09f9f4acd267724542ae
Enuel <enuel.monzon@gmail.com>
2021-07-19 16:44:54 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ShoppingCart from "./components/ShoppingCart";
+
+function App() {
+  return (
+    <div>
+      <h1>useReducer</h1>
+      <hr />
+      <ShoppingCart />
+    </div>
+  );
+}
+
+export default App;
